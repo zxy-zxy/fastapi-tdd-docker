b7ef39aa8d127caa98aafa58ec1a7b398c034852
ivan <sinitsinvanya@gmail.com>
2020-12-07 23:04:58 +0400

    added summaries api added summaries api tests

--- project/app/main.py
+++ project/app/main.py
@@ -3,9 +3,9 @@ import logging
 from logging import config
 
 from fastapi import FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
+from app.db import init_db
 
 logging_config_filepath = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "logging.ini"
@@ -16,18 +16,21 @@ logger = logging.getLogger(__name__)
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
+    application.include_router(summaries.router, prefix="/summaries", tags=["summaries"])
 
     return application
 
 
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Shutting down...")
